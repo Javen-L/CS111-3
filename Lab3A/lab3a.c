8c03442baab71274553bc707a7946cb24af47b0a
Anh Mac <anhmvc@gmail.com>
2020-06-02 06:01:24 -0700

    Added free blocks, free inodes, and inode summary functions

--- Lab3A/lab3a.c
+++ Lab3A/lab3a.c
@@ -9,9 +9,13 @@
 #include <sys/stat.h> // for open(3)
 #include <fcntl.h> // for open(3)
 #include <string.h>
+#include <time.h>
 #include "ext2_fs.h"
 
 #define SBLOCK_OFFSET 1024
+#define FILE 0x8000
+#define DIRECTORY 0x4000
+#define LINK 0xA000
 
 int mount = -1;
 int numGroups = 1; // according to spec, there is only 1 single group
@@ -22,10 +26,6 @@ __u32 block_size;
 void exit_with_error(char* message) {
     fprintf(stderr, "%s with error: %s\n", message, strerror(errno));
     exit(2);
-}
-
-__u32 group_offset(int groupNum) {
-    return block_size + SBLOCK_OFFSET + groupNum * sizeof(struct ext2_group_desc);
 }
 
 void printSB() {
@@ -46,25 +46,123 @@ void printSB() {
         superblock.s_first_ino);
 }
 
-void printGroups() {
-    int i;
-    struct ext2_group_desc group;
-    for (i = 0; i < numGroups; i++) {
-        if (pread(mount, &group_desc_table[i], sizeof(struct ext2_group_desc), group_offset(i)) < 0)
-            exit_with_error("Failed to read Block Group Descriptor Table");
+void printGroups(int num) {
+    struct ext2_group_desc group = group_desc_table[num];
+    fprintf(stdout, "GROUP,%u,%u,%u,%u,%u,%u,%u,%u\n",
+        num,
+        superblock.s_blocks_count,
+        superblock.s_inodes_count,
+        group.bg_free_blocks_count,
+        group.bg_free_inodes_count,
+        group.bg_block_bitmap,
+        group.bg_inode_bitmap,
+        group.bg_inode_table);
+}
 
-        group = group_desc_table[i];
-        fprintf(stdout, "GROUP,%u,%u,%u,%u,%u,%u,%u,%u\n",
-            i,
-            superblock.s_blocks_count,
-            superblock.s_inodes_count,
-            group.bg_free_blocks_count,
-            group.bg_free_inodes_count,
-            group.bg_block_bitmap,
-            group.bg_inode_bitmap,
-            group.bg_inode_table
-        );
+int block_offset(int num) {
+    return (num - 1) * (int) block_size + SBLOCK_OFFSET;
+}
+
+void printFreeBlocks(int num) {
+    struct ext2_group_desc group = group_desc_table[num];
+    char* bytes = malloc(block_size * sizeof(char));
+    if (pread(mount, bytes, block_size, block_offset(group.bg_block_bitmap)) < 0)
+        exit_with_error("Failed to read Block Bitmap");
+
+    int i,j;
+    int block = 1; // block number starts from 1 (EXT2 docs)
+    for (i = 0; i < (int) block_size; i++) {
+        char byte = bytes[i];
+        for (j = 0; j < 8; j++) { // read each bit of the byte
+            int free = !(byte & 1); // 1 means "used", 0 means "free/available"
+            if (free)
+                fprintf(stdout, "BFREE,%d\n", block);
+            byte = byte >> 1;
+            block++;
+        }
     }
+    free(bytes);
+}
+
+int inode_offset(int num, int i) {
+    return block_offset(num) + i * sizeof(struct ext2_inode);
+}
+
+char formatFiletype(__u32 type) {
+    int mask = 0xF000;
+    if ((type&mask) == FILE)
+        return 'f';
+    if ((type&mask) == DIRECTORY)
+        return 'd';
+    if ((type&mask) == LINK)
+        return 's';
+    else
+        return '?';
+}
+
+void formatTime(__u32 time, char* buffer) {
+    time_t rawtime = time;
+    struct tm *info;
+    info = gmtime(&rawtime);
+    strftime(buffer, 20, "%m/%d/%y %H:%M:%S", info);
+}
+
+void printInode(__u32 inode_num, __u32 inodeTable) {      
+    struct ext2_inode inode;
+    if (pread(mount, &inode, sizeof(inode), inode_offset(inodeTable, inode_num-1)) < 0)
+        exit_with_error("Failed to read Inode");
+    
+    if (inode.i_mode != 0 && inode.i_links_count != 0) {
+        char filetype = formatFiletype(inode.i_mode);
+        char atime[20], ctime[20], mtime[20];
+        formatTime(inode.i_atime, atime);
+        formatTime(inode.i_ctime, ctime);
+        formatTime(inode.i_mtime, mtime);
+
+        fprintf(stdout, "INODE,%d,%c,%o,%d,%d,%d,%s,%s,%s,%d,%d\n",
+            inode_num, // inode number
+            filetype, // file type
+            inode.i_mode & 0x0FFF, // mode, lower 12-bits
+            inode.i_uid, // owner
+            inode.i_gid, // group
+            inode.i_links_count, // link count
+            atime, // time of last inode change
+            ctime, // modification time
+            mtime, // time of last access
+            inode.i_size, // file size
+            inode.i_blocks // number of blocks of disk space
+            );  
+    }
+}
+
+void printInodesSummary(int num) {
+    struct ext2_group_desc group = group_desc_table[num];
+    __u32 inodeTable = group.bg_inode_table;
+    int numBytes = superblock.s_inodes_per_group / 8;
+    char* bytes = malloc(numBytes * sizeof(char));
+    if (pread(mount, bytes, block_size, block_offset(group.bg_inode_bitmap)) < 0)
+        exit_with_error("Failed to read Inode Bitmap");
+
+    int i,j;
+    __u32 inode_num = 1; // inode number starts from 1 (EXT2 docs)
+    for (i = 0; i < numBytes; i++) {
+        char byte = bytes[i];
+        for (j = 0; j < 8; j++) { // read each bit of the byte
+            int free = !(byte & 1); // 1 means "used", 0 means "free/available"
+            if (free)
+                fprintf(stdout, "IFREE,%d\n", inode_num);
+            else // allocated
+                printInode(inode_num, inodeTable);
+                
+            byte = byte >> 1;
+            inode_num++;
+        }
+    }
+    free(bytes);
+}
+
+int group_offset(int num) {
+    return (int) block_size + SBLOCK_OFFSET + num * sizeof(struct ext2_group_desc);
 }
 
 int main(int argc, char *argv[]){
@@ -79,7 +177,16 @@ int main(int argc, char *argv[]){
 
     printSB();
     group_desc_table = malloc(numGroups * sizeof(struct ext2_group_desc));
-    printGroups();
+    
+    int i;
+    for (i = 0; i < numGroups; i++) {
+        if (pread(mount, &group_desc_table[i], sizeof(struct ext2_group_desc), group_offset(i)) < 0)
+            exit_with_error("Failed to read Block Group Descriptor Table");
+
+        printGroups(i);
+        printFreeBlocks(i);
+        printInodesSummary(i);
+    }
     free(group_desc_table);
     exit(0);
 }
